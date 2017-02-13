9a0388e5d4d0a63043fdbb95b7794e1a9a8c1dbd
Brandon Leichty <brandonl@me.com>
2017-02-12 21:54:29 -0600

    add beginning of getShirtURL Promise function

--- app.js
+++ app.js
@@ -7,3 +7,50 @@ const request = require('request');
 const moment = require('moment');
 const fs = require('fs');
 const http = require('http');
+
+
+//URLs for the website shirts4mike.com
+const rootURL = 'http://shirts4mike.com/'
+const shirtURL = 'http://shirts4mike.com/shirts.php';
+
+//Create new date with proper format using "moment" npm package
+const date = moment().format("YYYY-MM-DD");
+
+
+
+const getShirtURL = new Promise(function(resolve, reject) {
+
+  //Checks to see if http://shirts4mike.com/shirts.php can be accessed
+  request(url, function (error, response, body) {
+    if (!error) {
+      resolve(body);
+    } else {
+      reject(error);
+    }
+  }
+});
+//END of getShirtURL Promise
+
+
+
+
+
+// A Basic Promise Example / https://davidwalsh.name/promises
+//
+// var p = new Promise(function(resolve, reject) {
+//
+// 	// Do an async task async task and then...
+//
+// 	if(/* good condition */) {
+// 		resolve('Success!');
+// 	}
+// 	else {
+// 		reject('Failure!');
+// 	}
+// });
+//
+// p.then(function() {
+// 	/* do something with the result */
+// }).catch(function() {
+// 	/* error :( */
+// })
