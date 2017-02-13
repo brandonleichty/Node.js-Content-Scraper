c11d8d7f665d7411f94bda61baeae25b531944af
Brandon Leichty <brandonl@me.com>
2017-02-12 19:41:17 -0600

    add dependancies to app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+//Dependancies
+const cheerio = require('cheerio');
+const fastcsv = require('fast-csv');
+const request = require('request');
+const moment = require('moment');
+const fs = require('fs');
+const http = require('http');
